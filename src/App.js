856d3e156961b70f842613b8239dfdbaca84e668
NadavMozeson <nadavson02@gmail.com>
2025-02-25 19:55:35 +0200

    Create login and signup page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/Landing.page';
+import LoginPage from './pages/Login.page';
+import SignupPage from './pages/Signup.page';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
       </Routes>
     </Router>
   );
