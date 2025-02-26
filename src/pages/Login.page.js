9be7a77bf368d07496f2e9bf0559ccbfd125982f
NadavMozeson <nadavson02@gmail.com>
2025-02-26 17:43:49 +0200

    Changed signup page css to work also on login

--- src/pages/Login.page.js
+++ src/pages/Login.page.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/AuthPage.style.css';
 
 function LoginPage() {
   const [formData, setFormData] = useState({
