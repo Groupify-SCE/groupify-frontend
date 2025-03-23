776bfa8198bb2d6860c9d92cff906b47db8a7a5b
MaorMalka <maor4211@gmail.com>
2025-03-23 17:58:50 +0200

    Add a + button to add a project

--- src/services/project.service.js
+++ src/services/project.service.js
@@ -1,25 +1,25 @@
 import { BackendManager } from './backend.manager';
 
 class ProjectService extends BackendManager {
-    static instance;
-  
-    constructor() {
-      super('projects');
-    }
-  
-    static getInstance() {
-      if (!ProjectService.instance) {
-        ProjectService.instance = new ProjectService();
-      }
-      return ProjectService.instance;
-    }
+  static instance;
 
-    async create() {
-        return await this.sendRequest('/create', {
-          credentials: 'include',
-          method: 'POST',
-        });
-      }
+  constructor() {
+    super('projects');
+  }
+
+  static getInstance() {
+    if (!ProjectService.instance) {
+      ProjectService.instance = new ProjectService();
+    }
+    return ProjectService.instance;
+  }
+
+  async create() {
+    return await this.sendRequest('/create', {
+      credentials: 'include',
+      method: 'POST',
+    });
+  }
 }
 
 const projectService = ProjectService.getInstance();
